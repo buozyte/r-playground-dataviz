50d293e6d6830cc54d26d8a2eb7c84dd722fa2f0
Donata Buozyte <donata.buozyte@tum.de>
2021-10-21 14:40:43 +0200

    Fix up tutorial :)

--- sheet1/tutorial.R
+++ sheet1/tutorial.R
@@ -9,28 +9,29 @@ library(dslabs)
 # section 01
 # 1
 n <- 100
-print(n * (n + 1) / 2)
+formula_100 <- (n * (n + 1) / 2)
 
 # 2
 n <- 1000
-print(n * (n + 1) / 2)
+formula_1000 <- (n * (n + 1) / 2)
 
 # 3
 n <- 1000
 x <- seq(1,n)
-print(sum(x))            # == n * (n + 1) / 2
-# Answer to question: b
+summed <- sum(x)            # == n * (n + 1) / 2
+# Answer to question: b (+1)
 
 # 4
-log(sqrt(100), base = 10)
+v1 <- log(sqrt(100), base = 10)
+v2 <- log10(sqrt(100))
 
 # 5
-# Answer: c
+# Answer: c (+1)
 
 # 6
 x <- seq(1,100)
-sum(x ^ (-2))
-(pi ^ 2) / 6
+summed_arr <- sum(x ^ (-2))
+form_pi <- (pi ^ 2) / 6
 
 # ---
 
@@ -38,22 +39,23 @@ sum(x ^ (-2))
 # 1
 data(murders)
 str(murders)
-# Answer: c
+# Answer: c (+1)
 
 # 2
-names(murders)
+name_m <- names(murders)
+name_col_m <- colnames(murders)     # specification sometimes needed, i.e. is rows and cols have names
 
 # 3
 a <- murders$abb
-class(a)
+class_a <- class(a)
 
 # 4
-length(levels(murders$region))
+num_reg <- length(levels(murders$region))
 
 # 5
 x <- c('DC', 'Alabama', 'Florida', 'Florida', 'DC', 'DC')
-table(x)
-table(murders$region)
+rep_x <- table(x)
+rep_reg <- table(murders$region)
 
 # ---
 
@@ -68,20 +70,20 @@ city <- c('Beijing', 'Lagos', 'Paris', 'Rio de Janeiro', 'San Juan', 'Toronto')
 names(temp) <- city
 
 # 4
-temp[1:3]
+first_3 <- temp[1:3]
 
 # 5
-temp[c('Paris', 'San Juan')]
+spec_city <- temp[c('Paris', 'San Juan')]
 
 # 6
-12:73
+arr <- 12:73
 
 # 7
-seq(1,100,2)
+odd_up_to_100 <- seq(1,100,2)
 
 # 8
 vec <- seq(6,55,4/7)
-length(vec)
+numbers_in_vec <- length(vec)
 
 # 9
 class(seq(1,10,0.5))
@@ -97,4 +99,4 @@ class(a<-1L)
 
 # 12
 x <- c("1", "3", "5")
-as.integer(x)
+casted_x <- as.integer(x)
