5332c789deee4646cb8f7fb5ec2077bdfb77c8e5
Donata Buozyte <donata.buozyte@tum.de>
2021-11-03 16:44:53 +0100

    Update: lecture tidy data

---
+++ sheet3/quizzes.R
@@ -0,0 +1,16 @@
+# Quizzes
+
+# Question 1
+# Answer: yes (+1)
+
+# Question 2
+# Answer: no, values (1999, 2000) in columns! (+1)
+
+# Question 3
+# Answer: melt (2) (+1)
+
+# Question 4
+# Answer: melt, unite and cast (4) (+1)
+
+# Question 5
+# Answer: Right (4) (+1)
